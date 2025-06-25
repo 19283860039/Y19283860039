1b073b9d7d0434d0a38759836e3405d72f37b303
19283860039 <2711224160@qq.com>
2025-06-25 21:25:07 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,22 @@
+import warnings, os
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+import torch
+torch.cuda.empty_cache()
+
+if __name__ == '__main__':
+    torch.cuda.empty_cache()
+
+    model = YOLO('ultralytics/cfg/models/12/yolo12m.yaml')
+    # model.load('yolo11n.pt') # loading pretrain weights
+    model.train(data='YAML file path',
+                cache=False,
+                imgsz=640,
+                epochs=300,
+                batch=32,
+                close_mosaic=0, 
+                workers=8, 
+                optimizer='SGD', 
+                project='runs/train',
+                name='exp',
+                )
